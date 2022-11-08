2330e4c4ae3a5a11e5a541b8a3c04c25d7662bfd
HassanKhan123 <hk2602809@gmail.com>
2022-11-08 13:09:57 +0500

    added: orders route

--- apps/orders/src/orders.service.ts
+++ apps/orders/src/orders.service.ts
@@ -1,8 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { lastValueFrom } from 'rxjs';
+import { CreateOrderRequest } from './dto/create-order.request';
+import { OrdersRepository } from './orders.repository';
 
 @Injectable()
 export class OrdersService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly ordersRepository: OrdersRepository) {}
+
+  async createOrder(request: CreateOrderRequest) {
+    // const session = await this.ordersRepository.startTransaction();
+    try {
+      const order = await this.ordersRepository.create(request);
+      // await session.commitTransaction();
+      return order;
+    } catch (err) {
+      // await session.abortTransaction();
+      throw err;
+    }
+  }
+
+  async getOrders() {
+    return this.ordersRepository.find({});
   }
 }
