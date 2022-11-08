ddf70fe2feee41ab449e5131be6bf34a5f41e684
HassanKhan123 <hk2602809@gmail.com>
2022-11-08 14:49:55 +0500

    rabbitmq service added

--- apps/orders/src/orders.service.ts
+++ apps/orders/src/orders.service.ts
@@ -1,20 +1,31 @@
 import { Inject, Injectable } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
 import { lastValueFrom } from 'rxjs';
+import { BILLING_SERVICE } from './contants/services';
 import { CreateOrderRequest } from './dto/create-order.request';
 import { OrdersRepository } from './orders.repository';
 
 @Injectable()
 export class OrdersService {
-  constructor(private readonly ordersRepository: OrdersRepository) {}
+  constructor(
+    private readonly ordersRepository: OrdersRepository,
+    @Inject(BILLING_SERVICE) private billingClient: ClientProxy,
+  ) {}
 
   async createOrder(request: CreateOrderRequest) {
-    // const session = await this.ordersRepository.startTransaction();
+    const session = await this.ordersRepository.startTransaction();
     try {
-      const order = await this.ordersRepository.create(request);
-      // await session.commitTransaction();
+      const order = await this.ordersRepository.create(request, { session });
+      await lastValueFrom(
+        this.billingClient.emit('order_created', {
+          request,
+          // Authentication: authentication,
+        }),
+      );
+      await session.commitTransaction();
       return order;
     } catch (err) {
-      // await session.abortTransaction();
+      await session.abortTransaction();
       throw err;
     }
   }
